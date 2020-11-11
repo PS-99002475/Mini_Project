84f0508481ea13fc119716ea100b032a23a539e7
99002473 <bibekkumar.patro@ltts.com>
2020-11-11 23:52:02 +0530

    appending the delay function to initial build code

--- Miniproject_SoulLock/3_Implementation/pirlock.c
+++ Miniproject_SoulLock/3_Implementation/pirlock.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 /*
  *gpio_26 - clockwise(open):Relay1:output
  *gpio_44 - anticlockwise(close):Relay2:output
  *gpio_68 - anticlockwise(sense):PIR:Input
 */
+
+void delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+}
 
 int main()
 {
